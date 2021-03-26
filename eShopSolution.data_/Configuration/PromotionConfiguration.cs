a8916933640024945806f9215471b8223cd53704
nguyenthanhnhan62000 <67071785+nguyenthanhnhan62000@users.noreply.github.com>
2021-03-26 12:26:11 +0700

    adđ

---
+++ eShopSolution.data_/Configuration/PromotionConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.data_.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.data_.Configuration
+{
+    public class PromotionConfiguration : IEntityTypeConfiguration<Promotion>
+    {
+        public void Configure(EntityTypeBuilder<Promotion> builder)
+        {
+            builder.ToTable("Promotions");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+            builder.Property(x => x.Name).IsRequired();
+        }
+    }
+}
